27155b93e27f725eae2017f34fc05671a113d581
brandzess123 <{lolikimngok@gmail.com}>
2023-02-13 21:16:46 +0700

    sss

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,28 +9,69 @@ import { ReactComponent as Vectors } from "../assets/svg/vector.svg";
 import { ReactComponent as IconRoundFooter } from "../assets/svg/Icon_Round_footer.svg";
 
 import { ReactComponent as MessFooter } from "../assets/svg/messenger_footer.svg";
-import { ReactComponent as GmailFooter } from "../assets/svg/GmailFooter.svg"; //GmailFooter -> 
+import { ReactComponent as GmailFooter } from "../assets/svg/GmailFooter.svg"; //GmailFooter ->
 import { ReactComponent as Icon } from "../assets/svg/Icon.svg";
 import { ReactComponent as SidePicture } from "../assets/svg/SidePicture.svg";
 import { ReactComponent as SidePicture2 } from "../assets/svg/SidePicture2.svg";
 import { ReactComponent as SidePictureRound } from "../assets/svg/SidePictureRound.svg";
+
+//welcome go stock
+import { ReactComponent as IconDollar } from "../assets/svg/WelcomeGoStock/IconDollar.svg";
+import { ReactComponent as Vector_0 } from "../assets/svg/WelcomeGoStock/Vector.svg";
+import { ReactComponent as Vector_1 } from "../assets/svg/WelcomeGoStock/Vector_1.svg";
+import { ReactComponent as Vector_2 } from "../assets/svg/WelcomeGoStock/Vector_2.svg";
+import { ReactComponent as Vector_3 } from "../assets/svg/WelcomeGoStock/Vector_3.svg";
+import { ReactComponent as Vector_4 } from "../assets/svg/WelcomeGoStock/Vector_4.svg";
+import { ReactComponent as Vector_5 } from "../assets/svg/WelcomeGoStock/Vector_5.svg";
+import { ReactComponent as Vector_6 } from "../assets/svg/WelcomeGoStock/Vector_6.svg";
+import { ReactComponent as Graph } from "../assets/svg/WelcomeGoStock/graph.svg";
+import { ReactComponent as VinHome } from "../assets/svg/WelcomeGoStock/Icon-Vinhomes.svg";
+import { ReactComponent as Apax } from "../assets/svg/WelcomeGoStock/Icon-Apax.svg";
 // import Board from "../assets/img/Board.jpg";
 
 function LandingPage() {
+  const items = [
+    {
+      Icon: Vector_0,
+      name: "Báo cáo",
+    },
+    {
+      Icon: Vector_1,
+      name: "Portfolio",
+    },
+    {
+      Icon: Vector_2,
+      name: "Khám phá",
+    },
+    {
+      Icon: Vector_3,
+      name: "Thanh toán",
+    },
+    {
+      Icon: Vector_4,
+      name: "Lịch sử",
+    },
+    {
+      Icon: Vector_5,
+      name: "Tin nhắn",
+    },
+    {
+      Icon: Vector_6,
+      name: "Cài đặt",
+    },
+  ];
+
   return (
     <React.Fragment>
       <div className="w-full h-screen overflow-auto bg-white  ">
-        
         <nav className="text-color font-lato m-0 p-0">
           <div className="max-w-full w-full h-56  bg-white text-color font-lato relative">
             <Decorate className="absolute" />
 
-
             <div className="flex flex-nowrap max-w-full justify-between items-center border-yellow-300    ">
-              
               <div className="flex  flex-nowrap ml-6 my-8  ">
                 <div className="   mr-2  h-9  overflow-hidden">
-                     <Icon className="h-full"></Icon>
+                  <Icon className="h-full"></Icon>
                 </div>
 
                 <a href="#" className="text-color text-3xl font-bold">
@@ -82,15 +123,10 @@ function LandingPage() {
           </div>
         </nav>
 
-         
-         <div className="relative  left-[80%] ">
-              <SidePicture className="absolute  ">
-             </SidePicture>
-         </div>
+        <div className="relative  left-[80%] ">
+          <SidePicture className="absolute  "></SidePicture>
+        </div>
 
-         
-
-       
         {/* //height */}
         <main className="w-full  h-auto text-color  justify-center items-center bg-white ">
           <section>
@@ -100,7 +136,8 @@ function LandingPage() {
             </h1>{" "}
           </section>
 
-          <form className="flex flex-nowrap mx-auto justify-center mt-6 w-2/5 border-red-400">
+          {/* bắt đầu */}
+          <form className="flex flex-nowrap mx-auto justify-center mt-6 w-2/5 border-red-400 ">
             <input
               className="w-2/3 h-14 border border-r-0  pl-2 rounded-lg rounded-r-none "
               type="text"
@@ -118,26 +155,118 @@ function LandingPage() {
             </button>
           </form>
 
-          <div className=" mx-auto  w-3/4  h-[700px] bg-cover overflow-hidden mt-10 border ">
+          <div className=" mx-auto  w-5/6  h-[900px] bg-cover overflow-hidden mt-10 border relative">
             {/* <Multiple_Board1 className="w-full left-0 top-0   "></Multiple_Board1> */}
+
+            <div className="absolute w-[84%] border h-[800px] ml-16 rounded-2xl flex justify-center items-center mt-2 font-roboto ">
+              {/* div cha */}
+              <div className="w-[90%] border h-[91%]  rounded-2xl grid grid-cols-3 font-roboto overflow-hidden">
+                <div className="col-span-1   bg-main-picture p-6  ">
+                  <div className="flex mb-10   ">
+                    <IconDollar className="mr-2 w-[50px] h-[35px]"></IconDollar>
+                    <h1 className="text-2xl">Gostock</h1>
+                  </div>
+
+                  <button className="w-[80%] h-[53px] mb-16  bg-main-button-picture text-center rounded-lg  text-white">
+                    Đầu tư ngay
+                  </button>
+
+                  <ul>
+                    {items.map(({ Icon, name }, index) => (
+                      <li key={index} className="flex mb-8   ml-3">
+                        <Icon className="w-[15px] mr-4 h-[15px]" />
+                        <h2 className="bg-side-color text-sm">{name}</h2>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+
+                {/* welcome go stock */}
+                <div className="col-span-2 border p-6">
+                  <h2 className="font-lato  font-bold text-xl  font-side-color">
+                    Chào mừng bạn tới với{" "}
+                    <h2 className="inline bg-font-color font-lato">
+                      Gostock Platform
+                    </h2>
+                  </h2>
+                  <p className="font-inters text-base mt-5 bg-side-color ">
+                    Chào buổi sáng,{" "}
+                    <p className="font-inters inline bg-font-color">
+                      Việt Hưng
+                    </p>
+                  </p>
+
+                  <p className="mt-10 font-side-color2 font-lato font-bold">
+                    Tóm tắt Portfolio
+                  </p>
+
+                  <div className="flex w-auto  mt-7  ">
+                    <p className="bg-side-color mr-4 ">Thời gian</p>
+
+                    <select
+                      name="date"
+                      id="date"
+                      className="inline-block w-[35%] border rounded-lg font-side-color2 text-center font-bold"
+                    >
+                      <option value="T10-2020">T10-2020</option>
+                      <option value="saab">Saab</option>
+                    </select>
+                  </div>
+
+                  <div className="mt-9 w-[99%] h-auto overflow-hidden">
+                    <Graph className="w-full"></Graph>
+                  </div>
+
+                  <div className="w-[95%] h-[70px] mt-4 mx-auto rounded-lg shadow-lg border flex flex-nowrap p-3">
+                    <VinHome></VinHome>
+
+                    <div className="flex flex-col ml-2">
+                      <h5 className="font-lato font-bold text-xs">VinHomes</h5>
+                      <p className=" bg-side-color text-ssm">Bất động sản</p>
+                    </div>
+
+                    <div className="flex flex-col ml-20">
+                      <h4 className="font-lato font-bold text-xs">VND 50.000.000</h4>
+                      <p className=" bg-side-color text-ssm">Investment Value</p>
+                    </div>
+
+                    <h2 className="font-lato font-bold text-xs ml-16 bg-font-color" >+10.75%</h2>
+
+
+                  </div>
+                 
+                  <div className="w-[95%] h-[70px] mt-4 mx-auto rounded-lg shadow-lg border flex flex-nowrap p-3">
+                    <Apax></Apax>
+
+                    <div className="flex flex-col ml-2">
+                      <h5 className="font-lato font-bold text-xs">APAX</h5>
+                      <p className=" bg-side-color text-ssm">Teaching Platform</p>
+                    </div>
+
+                    <div className="flex flex-col ml-[72px]">
+                      <h4 className="font-lato font-bold text-xs">VND 50.000.000</h4>
+                      <p className=" bg-side-color text-ssm">Investment Value</p>
+                    </div>
+
+                    <h2 className="font-lato font-bold text-xs ml-16 bg-font-color" >+4.75%</h2>
+
+
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
-
-
           <div className="relative  right-[20%] top-[64px]  ">
-              <SidePicture2 className="absolute  ">
-             </SidePicture2>
-         </div>
-
+            <SidePicture2 className="absolute  "></SidePicture2>
+          </div>
 
           <div className="mt-10 mx-auto w-3/4 grid grid-cols-2 border ">
             <div className="grid-cols-1 w-full  border h-[600px] overflow-hidden ">
               {/* <Left_chart className="w-full border"></Left_chart> */}
             </div>
 
-              
-
-          
             <section className="grid-cols-1 p-10">
               <h1 className="font-inters  leading-[3rem] font-medium text-4xl text-main-green">
                 Dễ dàng theo giõi các biến động cổ phiếu với{" "}
@@ -159,11 +288,9 @@ function LandingPage() {
             </section>
           </div>
 
-
           <div className="relative  left-[79%] ">
-              <SidePictureRound className="absolute  ">
-             </SidePictureRound>
-         </div>
+            <SidePictureRound className="absolute  "></SidePictureRound>
+          </div>
 
           <div className="mt-10 mx-auto w-3/4 grid grid-cols-2 border  ">
             <section className="grid-cols-1 p-10">
@@ -194,7 +321,7 @@ function LandingPage() {
           </div>
 
           <div className="mt-2 w-full overflow-hidden h-[220px] relative ">
-            <Vectors className="w-full absolute"></Vectors>
+            <Vectors className="w-full absolute "></Vectors>
           </div>
 
           <div className="mt-10 mx-auto w-3/4 grid grid-cols-11 border ">
@@ -218,7 +345,6 @@ function LandingPage() {
           </div>
 
           <div className="mt-10 mx-auto w-3/4 grid grid-cols-2 border ">
-           
             <section className="grid-cols-1 p-10">
               <h1 className="mt-10 font-inters  leading-[2rem] font-medium text-3xl text-main-green">
                 Phân tích mọi cổ phiếu chỉ trong 3 phút
@@ -257,12 +383,10 @@ function LandingPage() {
         </main>
 
         <footer className=" w-full h-auto  bg-footer-color  flex justify-center p-16">
-         
           <div className="w-[86%] grid grid-cols-11  ">
             <nav className="col-span-7 pl-6 font-roboto text-white flex flex-col w-full text-lg">
-             
               <div className="  h-14 w-full flex  align-center mb-12 ">
-                <IconRoundFooter className="h-full "></IconRoundFooter> 
+                <IconRoundFooter className="h-full "></IconRoundFooter>
                 <a
                   href=""
                   className="p-3 font-lato text-3xl text-white drop-shadow-md  "
@@ -271,24 +395,32 @@ function LandingPage() {
                 </a>
               </div>
 
-              <p className=" text-white font-bold">
-                CÔNG TY CỔ PHẦN GoStock
-              </p>
+              <p className=" text-white font-bold">CÔNG TY CỔ PHẦN GoStock</p>
               <p className=" text-white">MST: 0109620361</p>
               <p className=" text-white">Số 5 ngõ 316 Lê Trọng Tấn</p>
             </nav>
 
-
             <div className="col-span-4 ">
               <nav className="flex flex-wrap  box-border   justify-center font-inter text-base ">
-                <a href="#"  className="text-white m-5 hover:text-sky-700">Về chúng tôi</a>
-                <a href="#"  className="text-white m-5 hover:text-sky-700">Điều Khoản Sử Dụng</a>
-                <a href="#"  className="text-white m-5 hover:text-sky-700">Gói Hội Viên</a>
-                <a href="#"  className="text-white m-5 hover:text-sky-700">Chính Sách Bảo mật</a>
+                <a href="#" className="text-white m-5 hover:text-sky-700">
+                  Về chúng tôi
+                </a>
+                <a href="#" className="text-white m-5 hover:text-sky-700">
+                  Điều Khoản Sử Dụng
+                </a>
+                <a href="#" className="text-white m-5 hover:text-sky-700">
+                  Gói Hội Viên
+                </a>
+                <a href="#" className="text-white m-5 hover:text-sky-700">
+                  Chính Sách Bảo mật
+                </a>
               </nav>
 
               <nav className=" flex justify-center  flex-col  font-inter text-base ">
-                <a href="#" className="text-white m-5 self-center hover:text-sky-700">
+                <a
+                  href="#"
+                  className="text-white m-5 self-center hover:text-sky-700"
+                >
                   Liên hệ với chúng tôi
                 </a>
 
