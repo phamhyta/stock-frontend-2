e65635e8a78b5b5a979bc9713840531f487babc6
brandzess123 <{lolikimngok@gmail.com}>
2023-02-14 22:28:19 +0700

    third commit

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -27,7 +27,14 @@ import { ReactComponent as Vector_6 } from "../assets/svg/WelcomeGoStock/Vector_
 import { ReactComponent as Graph } from "../assets/svg/WelcomeGoStock/graph.svg";
 import { ReactComponent as VinHome } from "../assets/svg/WelcomeGoStock/Icon-Vinhomes.svg";
 import { ReactComponent as Apax } from "../assets/svg/WelcomeGoStock/Icon-Apax.svg";
+import {ReactComponent as TechComBank} from "../assets/svg/WelcomeGoStock/TechComBank.svg";
+import {ReactComponent as  RoundCircle} from "../assets/svg/WelcomeGoStock/RoundCircle.svg";
+
 // import Board from "../assets/img/Board.jpg";
+
+//first left side
+import {ReactComponent as  LeftGraph} from "../assets/svg/LeftSide/LeftGraph.svg";
+import {ReactComponent as  MonthlyView} from "../assets/svg/LeftSide/MonthlyView.svg";
 
 function LandingPage() {
   const items = [
@@ -60,6 +67,47 @@ function LandingPage() {
       name: "Cài đặt",
     },
   ];
+
+  const cophieu = [
+    {
+      name: "FPT Corp",
+      unit: "77,200",
+      price: "+0.39%",
+    },
+    {
+      name: " Vingroup JSC",
+      unit: "71,200",
+      price: "+1.86%",
+    },
+    {
+      name: " Vinhomes",
+      unit: "55,400",
+      price: " -0.18%",
+    },
+    {
+      name: " HGM",
+      unit: "40,000",
+      price: " +0.00%",
+    },
+    {
+      name: "Techcombank",
+      unit: "28,000",
+      price: " +0.35%",
+    },
+    {
+      name: "GTNFoods JSC",
+      unit: "19,100",
+      price: " +0.00%",
+    },
+    {
+      name: " APAX",
+      unit: "6,120.0",
+      price: " -6.99%",
+    },
+  ];
+  const tag = "<";
+
+  const widthpage = "88%";
 
   return (
     <React.Fragment>
@@ -155,12 +203,9 @@ function LandingPage() {
             </button>
           </form>
 
-          <div className=" mx-auto  w-5/6  h-[900px] bg-cover overflow-hidden mt-10 border relative">
-            {/* <Multiple_Board1 className="w-full left-0 top-0   "></Multiple_Board1> */}
-
-            <div className="absolute w-[84%] border h-[800px] ml-16 rounded-2xl flex justify-center items-center mt-2 font-roboto ">
-              {/* div cha */}
-              <div className="w-[90%] border h-[91%]  rounded-2xl grid grid-cols-3 font-roboto overflow-hidden">
+          <div className=" mx-auto  w-[85%]  h-[900px] bg-cover overflow-hidden mt-10  relative">
+            <div className="absolute w-[78%]  h-[800px] ml-12 rounded-2xl flex justify-center items-center mt-2 font-roboto">
+              <div className="w-[91%] border h-[91%]  rounded-2xl grid grid-cols-3 font-roboto overflow-hidden">
                 <div className="col-span-1   bg-main-picture p-6  ">
                   <div className="flex mb-10   ">
                     <IconDollar className="mr-2 w-[50px] h-[35px]"></IconDollar>
@@ -181,9 +226,8 @@ function LandingPage() {
                   </ul>
                 </div>
 
-
                 {/* welcome go stock */}
-                <div className="col-span-2 border p-6">
+                <div className="col-span-2  p-6 mr-[80px]">
                   <h2 className="font-lato  font-bold text-xl  font-side-color">
                     Chào mừng bạn tới với{" "}
                     <h2 className="inline bg-font-color font-lato">
@@ -227,48 +271,143 @@ function LandingPage() {
                     </div>
 
                     <div className="flex flex-col ml-20">
-                      <h4 className="font-lato font-bold text-xs">VND 50.000.000</h4>
-                      <p className=" bg-side-color text-ssm">Investment Value</p>
+                      <h4 className="font-lato font-bold text-xs">
+                        VND 50.000.000
+                      </h4>
+                      <p className=" bg-side-color text-ssm">
+                        Investment Value
+                      </p>
                     </div>
 
-                    <h2 className="font-lato font-bold text-xs ml-16 bg-font-color" >+10.75%</h2>
-
-
+                    <h2 className="font-lato font-bold text-xs ml-16 bg-font-color">
+                      +10.75%
+                    </h2>
                   </div>
-                 
+
                   <div className="w-[95%] h-[70px] mt-4 mx-auto rounded-lg shadow-lg border flex flex-nowrap p-3">
                     <Apax></Apax>
 
                     <div className="flex flex-col ml-2">
                       <h5 className="font-lato font-bold text-xs">APAX</h5>
-                      <p className=" bg-side-color text-ssm">Teaching Platform</p>
+                      <p className=" bg-side-color text-ssm">
+                        Teaching Platform
+                      </p>
                     </div>
 
                     <div className="flex flex-col ml-[72px]">
-                      <h4 className="font-lato font-bold text-xs">VND 50.000.000</h4>
-                      <p className=" bg-side-color text-ssm">Investment Value</p>
+                      <h4 className="font-lato font-bold text-xs">
+                        VND 50.000.000
+                      </h4>
+                      <p className=" bg-side-color text-ssm">
+                        Investment Value
+                      </p>
                     </div>
 
-                    <h2 className="font-lato font-bold text-xs ml-16 bg-font-color" >+4.75%</h2>
-
-
+                    <h2 className="font-lato font-bold text-xs ml-16 bg-font-color">
+                      +4.75%
+                    </h2>
                   </div>
                 </div>
               </div>
             </div>
+
+            <div className="absolute w-[26%] bg-white h-[450px] right-3 rounded-xl border shadow-lg  pointer-events-none top-[71px] ">
+              <div className="flex justify-between p-2 font-side-color-w mb-5">
+                <h2 className="font-side-color2 font-lato font-bold">
+                  Cổ phiếu tiềm năng
+                </h2>
+                <a href="#" className="text-sm">
+                  Xem tất cả
+                </a>
+              </div>
+
+              <table className="table-auto w-[96%] mx-auto  font-lato ">
+                <thead className="p-5">
+                  <tr className="text-xs h-12">
+                    <td className="w-[10%] font-side-color-wt">#</td>
+                    <td className="font-side-color2 font-bold">Tên</td>
+                    <td className="font-side-color-wt2 font-bold w-[35%]">
+                      Giá Tiền/Unit
+                    </td>
+                    <td className="footer-color w-[15%] ">Lợi tức</td>
+                  </tr>
+                </thead>
+
+              
+                <tbody>
+                  {cophieu.map((value, index) => (
+                    <tr key={index} className="h-12 text-xs">
+                      <td className="w-[10%] font-side-color-wt">
+                        {index + 1}
+                      </td>
+                      <td className="font-side-color2 font-bold">
+                        {value.name}
+                      </td>
+                      <td className="font-side-color-wt2 font-bold w-[35%]">
+                        {value.unit}
+                      </td>
+                      <td className="footer-color w-[15%] ">{value.price}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="absolute w-[30%] bottom-6 left-0  h-[248px] ">
+                   <TechComBank className="h-full"></TechComBank>
+            </div>
+
+            <div className="absolute w-[30%] bottom-7 right-2 bg-white border shadow-lg  h-[284px] rounded-lg">
+              <div className="flex justify-between p-4 font-lato">
+                <h2 className="text-base font-side-color-2 font-bold">Phân bố tài khoản</h2>
+                <p className="bg-font-color text-sm">Chi tiết</p>
+              </div>
+
+              <div className="w-[89%] mt-3 h-[65%] mx-auto">
+                <RoundCircle className="w-full"></RoundCircle>
+                   
+              </div>
+                  
+            </div>
+
+            
           </div>
 
           <div className="relative  right-[20%] top-[64px]  ">
             <SidePicture2 className="absolute  "></SidePicture2>
           </div>
 
-          <div className="mt-10 mx-auto w-3/4 grid grid-cols-2 border ">
-            <div className="grid-cols-1 w-full  border h-[600px] overflow-hidden ">
-              {/* <Left_chart className="w-full border"></Left_chart> */}
+
+
+
+
+
+          <div className="mt-10 mx-auto w-[85%] grid grid-cols-11 border  ">
+            <div className="col-span-6 w-full  border h-[600px] overflow-hidden  flex relative ">
+             
+              <div className="shadow-left w-[48%] ml-0 mr-[4%] h-[500px] border  relative rounded-3xl flex flex-col pl-6 font-lato bg-white">
+                <p className="main-second-color text-3xl mb-9 ">{tag}</p>
+                <h2 className="text-color text-xl">LienViet <br></br> PostBank</h2>
+                <h2 className="text-2xl font-bold">SGD 788.42</h2>
+                <p className="bg-font-color text-sm mb-2">+6.2%</p>
+                
+                <div className="w-[full] h-[40%] mb-2 "><LeftGraph className="w-full"></LeftGraph></div>
+                <p className="text-color text-sm font-bold">Today Volume</p>
+                <p className="text-color text-sm font-bold">343,323</p>
+              </div>
+
+              <div className="w-[48%] shadow-left shadow-middle   h-[500px] mt-10 border bg-yellow-400 relative rounded-3xl">
+              </div>
+
+              <div className="w-[25%] absolute h-[200px] left-[39%]  top-[32%] p-0 m-0">
+                <MonthlyView className="w-full m-0"></MonthlyView>
+
+              </div>
+
             </div>
 
-            <section className="grid-cols-1 p-10">
-              <h1 className="font-inters  leading-[3rem] font-medium text-4xl text-main-green">
+            <section className="col-span-5 p-10">
+              <h1 className="font-inters  mt-7 leading-[3rem] font-medium text-4xl text-main-green">
                 Dễ dàng theo giõi các biến động cổ phiếu với{" "}
                 <h1 className="font-medium text-4xl text-main-linear">
                   GoStock
@@ -320,11 +459,15 @@ function LandingPage() {
             <div className="grid-cols-1 "></div>
           </div>
 
-          <div className="mt-2 w-full overflow-hidden h-[220px] relative ">
+
+
+
+          <div className="mt-10 w-full h-[600px]  relative flex justify-center">
+          <div className="right-0 top-0 w-full overflow-hidden h-[220px] relative mx-auto">
             <Vectors className="w-full absolute "></Vectors>
           </div>
 
-          <div className="mt-10 mx-auto w-3/4 grid grid-cols-11 border ">
+          <div className="top-5 mx-auto w-[88%] grid grid-cols-11 h-[500px] border absolute ">
             <div className="col-span-6"></div>
 
             <section className="col-span-5 p-10">
@@ -343,6 +486,15 @@ function LandingPage() {
               </button>
             </section>
           </div>
+
+          </div>
+
+         
+
+
+
+
+
 
           <div className="mt-10 mx-auto w-3/4 grid grid-cols-2 border ">
             <section className="grid-cols-1 p-10">
@@ -382,7 +534,7 @@ function LandingPage() {
           </div>
         </main>
 
-        <footer className=" w-full h-auto  bg-footer-color  flex justify-center p-16">
+        <footer className=" w-full h-auto  bg-footer-color  flex justify-center p-7">
           <div className="w-[86%] grid grid-cols-11  ">
             <nav className="col-span-7 pl-6 font-roboto text-white flex flex-col w-full text-lg">
               <div className="  h-14 w-full flex  align-center mb-12 ">
